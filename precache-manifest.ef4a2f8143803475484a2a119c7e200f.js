11dbdddfb85da2dc6bdb60809c4eef2ef538442e
unknown <filipzynwala@gmail.com>
2019-12-13 13:29:18 +0100

    Updates

---
+++ precache-manifest.ef4a2f8143803475484a2a119c7e200f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edffdb086f9d55c5aabe58d45361ff3",
+    "url": "/FZynwala/zaka-monitor/index.html"
+  },
+  {
+    "revision": "9d8a81f9a7602892a780",
+    "url": "/FZynwala/zaka-monitor/static/css/main.e0c78999.chunk.css"
+  },
+  {
+    "revision": "cac90360a090d27e2141",
+    "url": "/FZynwala/zaka-monitor/static/js/2.9ef4e34b.chunk.js"
+  },
+  {
+    "revision": "9d8a81f9a7602892a780",
+    "url": "/FZynwala/zaka-monitor/static/js/main.1482e1d9.chunk.js"
+  },
+  {
+    "revision": "b6c158480d39bb8ce8e6",
+    "url": "/FZynwala/zaka-monitor/static/js/runtime-main.c2ffc97c.js"
+  }
+]);
