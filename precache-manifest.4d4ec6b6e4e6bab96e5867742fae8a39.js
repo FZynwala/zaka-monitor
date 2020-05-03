1b6e8979e1f5cdbf05bf202f3c7c10c60d4d16f5
Filip Zynwala <filipzynwala@gmail.com>
2020-05-03 19:03:26 +0200

    Updates

---
+++ precache-manifest.4d4ec6b6e4e6bab96e5867742fae8a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235bb229c29692a50de842852503345e",
+    "url": "/zaka-monitor/index.html"
+  },
+  {
+    "revision": "e576a5ed9bb6aa064979",
+    "url": "/zaka-monitor/static/css/main.e9609a27.chunk.css"
+  },
+  {
+    "revision": "5524f4b048ea3cde0791",
+    "url": "/zaka-monitor/static/js/2.36d298ce.chunk.js"
+  },
+  {
+    "revision": "602d605ef36bc3acd02f8329d01a8800",
+    "url": "/zaka-monitor/static/js/2.36d298ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e576a5ed9bb6aa064979",
+    "url": "/zaka-monitor/static/js/main.e8e4469c.chunk.js"
+  },
+  {
+    "revision": "cfd4b6c8bc87dd4e6217",
+    "url": "/zaka-monitor/static/js/runtime-main.834d9064.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zaka-monitor/precache-manifest.bb3cb639f102466b778ba4f83450be03.js"
+  "/zaka-monitor/precache-manifest.4d4ec6b6e4e6bab96e5867742fae8a39.js"
 );
 
 self.addEventListener('message', (event) => {
