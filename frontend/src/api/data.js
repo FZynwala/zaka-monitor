7280653c4dff515c08a37dd35df11d9d947c502b
Filip Zynwala <filipzynwala@gmail.com>
2020-06-26 21:34:55 +0200

    Change herokuURL

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const herokuURL = 'https://young-ocean-79520.herokuapp.com/';
+const herokuURL = 'https://enigmatic-hamlet-94586.herokuapp.com/';
 const localhost = 'http://localhost:8000';
 
 export default axios.create({
