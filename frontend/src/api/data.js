ef1daf9c13ee9ee9083e3194c950323d50328ef8
Filip Zynwala <filipzynwala@gmail.com>
2023-01-02 12:40:35 +0100

    api

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const herokuURL = 'https://desolate-tor-57356.herokuapp.com/';
 const localhost = 'http://localhost:8000';
 const renderUrl = 'https://zaka-monitor-api.onrender.com';
+const devRenderUrl = 'https://dev-zaka-monitor-api.onrender.com';
 
 export default axios.create({
-    baseURL: localhost,
+    baseURL: devRenderUrl,
 });
